e715e2e7635f02189d9662ef7579906ce257b143
Luis Goyes <luis.goyes117@gmail.com>
2021-03-19 16:43:03 -0500

    Search View Controller ready

--- LyricSnitcher/Search/Presentation/SearchViewController.m
+++ LyricSnitcher/Search/Presentation/SearchViewController.m
@@ -43,36 +43,41 @@
 }
 
 - (void)hideArtistError {
-    
+    _artistTextField.backgroundColor = UIColor.systemBackgroundColor;
 }
 
 
 - (void)hideSongError {
-    
+    _songTextField.backgroundColor = UIColor.systemBackgroundColor;
 }
 
 
 - (void)setLoadingState {
-    
+    [_getLyricsButton setTitle:@"" forState:UIControlStateNormal];
+    [_loadingLyricsActivityIndicator setHidden:false];
 }
 
 
 - (void)setSteadyState {
-    
+    [_getLyricsButton setTitle:@"Get Lyrics" forState:UIControlStateNormal];
+    [_loadingLyricsActivityIndicator setHidden:true];
 }
 
 
 - (void)showArtistError {
-    
+    _artistTextField.backgroundColor = UIColor.systemRedColor;
 }
 
 
 - (void)showSongError {
-    
+    _songTextField.backgroundColor = UIColor.systemRedColor;
 }
 
 - (void)showError:(NSString *)message {
-    
+    UIAlertController * alert = [UIAlertController alertControllerWithTitle:@"Error" message:message preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction * action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:action];
+    [self presentViewController:alert animated:true completion:nil];
 }
 
 - (void)navigateToReader:(Lyrics *)lyrics {
